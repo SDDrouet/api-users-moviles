71261f84ec3bce846649dfd449a269f1e26effca
sddrouet <sddrouet@espe.edu.ec>
2024-11-16 03:16:51 -0500

    update sql

--- user_db.sql
+++ user_db.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
     `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `username` VARCHAR(50) NOT NULL UNIQUE,
     `password` VARCHAR(255) NOT NULL,
-    `email` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Crear un índice para acelerar las búsquedas por username
